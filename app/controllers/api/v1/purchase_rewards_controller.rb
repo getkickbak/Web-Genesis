5d09c514f28d1ebfa0dd5bd6b92ee0501c21bd01
Paul Chan <paul.chan@justforyourfriends.com>
2012-04-01 13:00:25 -0400

    prizes bug fix

--- app/controllers/api/v1/purchase_rewards_controller.rb
+++ app/controllers/api/v1/purchase_rewards_controller.rb
@@ -57,57 +57,66 @@ class Api::V1::PurchaseRewardsController < ApplicationController
             @vip_challenge = true
             @vip_points = challenge.points
           end
-          reward_ids = params[:reward_ids]
+          rewards_info = JSON.parse(params[:reward_ids])
+          reward_id_quantity = {}
+          reward_ids = []
+          rewards_info.each do |info|
+            reward_ids << info['id']
+            reward_id_quantity[info['id']] = info['quantity']
+          end
           @total_points = 0
           rewards = PurchaseReward.all(:id => reward_ids)
           rewards.each do |reward|
+            points = reward.points * reward_id_quantity[reward.id]
             record = EarnRewardRecord.new(
               :reward_id => reward.id,
               :venue_id => @venue.id,
-              :points => reward.points,
+              :points => points,
               :created_ts => now
             )
             record.merchant = @venue.merchant
             record.user = current_user
             record.save
-            @customer.points += reward.points
-            @total_points += reward.points
+            @customer.points += points
+            @total_points += points
           end
           @customer.save
           
           mutex = CacheMutex.new(@venue.merchant.cache_key, Cache.memcache)
           mutex.acquire
           reward_model = @venue.merchant.reward_model
-          prize = CustomerReward.get(reward_model.prize_reward_id) || pick_prize(@venue)
+          prize = CustomerReward.get(reward_model.prize_reward_id)
+          if prize.nil?
+            prize = pick_prize(@venue)
+            reward_model.prize_reward_id = prize.id
+            prize_interval = (prize.points / Float(reward_model.prize_rebate_rate) * 100).to_i
+            reward_model.prize_win_offset = pick_prize_win_offset(prize_interval)
+          else
+            prize_interval = (prize.points / Float(reward_model.prize_rebate_rate) * 100).to_i  
+          end
           current_point_offset = reward_model.prize_point_offset + @total_points
-          if (reward_model.prize_point_offset < reward_model.prize_win_offset) && (current_point_offset > reward_model.prize_win_offset)
+          if (reward_model.prize_point_offset < reward_model.prize_win_offset) && (current_point_offset >= reward_model.prize_win_offset)
+            earn_prize = EarnPrize.new(
+              :points => prize.points,
+              :expiry_date => 6.month.from_now,
+              :created_ts => now
+            )
+            earn_prize.reward = prize
+            earn_prize.merchant = @venue.merchant
+            earn_prize.user = current_user
+            earn_prize.save
+            @prizes << earn_prize          
+          end
+          while current_point_offset >= prize_interval
+            current_point_offset -= prize_interval
+            prize = pick_prize(@venue)
+            reward_model.prize_reward_id = prize.id
+            prize_interval = (prize.points / Float(reward_model.prize_rebate_rate) * 100).to_i  
+            reward_model.prize_win_offset = pick_prize_win_offset(prize_interval)  
+            if current_point_offset >= reward_model.prize_win_offset
               earn_prize = EarnPrize.new(
                 :points => prize.points,
                 :expiry_date => 6.month.from_now,
-                :created_ts => now
-              )
-              earn_prize.reward = prize
-              earn_prize.merchant = @venue.merchant
-              earn_prize.user = current_user
-              earn_prize.save
-              @prizes << earn_prize          
-          end
-          prize_interval = (prize.points / Float(reward_model.prize_rebate_rate) * 100).to_i
-          while current_point_offset >= prize_interval
-            prize = pick_prize(@venue)
-            reward_model.prize_reward_id = prize.id
-            if current_point_offset >= prize_interval
-              reward_model.prize_point_offset = current_point_offset - prize_interval
-            else
-              reward_model.prize_point_offset = current_point_offset
-            end  
-            prize_interval = (prize.points / Float(reward_model.prize_rebate_rate) * 100).to_i
-            reward_model.prize_win_offset = pick_prize_win_offset(prize_interval)  
-            reward_model.save
-            current_point_offset = reward_model.prize_point_offset - prize_interval
-            if current_point_offset > reward_model.prize_win_offset
-              earn_prize = EarnPrize.new(
-                :points => prize.points,
                 :created_ts => now
               )
               earn_prize.reward = prize
@@ -117,6 +126,8 @@ class Api::V1::PurchaseRewardsController < ApplicationController
               @prizes << earn_prize       
             end  
           end
+          reward_model.prize_point_offset = current_point_offset
+          reward_model.save
           mutex.release
           render :template => '/api/v1/purchase_rewards/earn'
         else
@@ -132,7 +143,8 @@ class Api::V1::PurchaseRewardsController < ApplicationController
           #format.xml  { render :xml => @referral.errors, :status => :unprocessable_entity }
           format.json { render :json => { :success => false, :message => ["Something went wrong", "Trouble completing the challenge.  Please try again."] } }
         end
-      rescue
+      rescue StandardError => e
+        logger.error("Exception: " + e.message)
         mutex.release if (defined? mutex && mutex)
         respond_to do |format|
           #format.xml  { render :xml => @referral.errors, :status => :unprocessable_entity }
