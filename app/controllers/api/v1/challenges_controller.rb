fd7429649dcf55ccec3267c34e9e5c545a224268
Paul Chan <paul.chan@justforyourfriends.com>
2012-04-14 16:27:51 -0400

    latest changes

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ChallengesController < ApplicationController
   
   def index
     authorize! :read, Challenge
+    
     @challenges = Challenge.all(Challenge.merchant.id => params[:merchant_id], :venues => Venue.all(:id => params[:venue_id]))
     render :template => '/api/v1/challenges/index'
   end
