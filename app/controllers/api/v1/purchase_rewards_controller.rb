13d6d2740d8b943acd9820dfef79294455c8d72e
Paul Chan <paul.chan@justforyourfriends.com>
2012-05-16 23:24:59 -0400

    bug fixes

--- app/controllers/api/v1/purchase_rewards_controller.rb
+++ app/controllers/api/v1/purchase_rewards_controller.rb
@@ -28,12 +28,17 @@ class Api::V1::PurchaseRewardsController < ApplicationController
         data = params[:data]
         cipher = Gibberish::AES.new(@venue.auth_code)
         decrypted = cipher.dec(data)
-        #logger.debug("decrypted text: #{decrypted}")
+        logger.debug("decrypted text: #{decrypted}")
         decrypted_data = JSON.parse(decrypted)
         now_secs = decrypted_data["expiry_ts"]/1000
         data_expiry_ts = Time.at(now_secs)
-        #logger.debug("decrypted expiry_ts: #{data_expiry_ts}")
-        if (decrypted_data["type"] == EncryptedDataType::EARN_POINTS) && (data_expiry_ts >= Time.now) && EarnRewardRecord.first(:venue_id => @venue.id, :data_expiry_ts => data_expiry_ts, :data => data).nil?
+        logger.debug("decrypted type: #{decrypted_data["type"]}")
+        logger.debug("decrypted expiry_ts: #{data_expiry_ts}")
+        logger.debug("decrypted data: #{data}")
+        logger.debug("Type comparison: #{decrypted_data["type"] == EncryptedDataType::EARN_POINTS}")
+        logger.debug("Time comparison: #{data_expiry_ts >= Time.now}")
+        logger.debug("EarnRewardRecord comparison: #{EarnRewardRecord.first(:venue_id => @venue.id, :data_expiry_ts.eq => data_expiry_ts, :data => data).nil?}")
+        if (decrypted_data["type"] == EncryptedDataType::EARN_POINTS) && (data_expiry_ts >= Time.now) && EarnRewardRecord.first(:venue_id => @venue.id, :data_expiry_ts.eq => data_expiry_ts, :data => data).nil?
           amount = decrypted_data["amount"].to_f
           logger.debug("Set authorized to true")
           authorized = true
