2c5e6b2908615e23887e667a58f23cd35e51d202
Paul Chan <paul.chan@justforyourfriends.com>
2012-05-23 16:36:55 -0400

    bug fixes

--- app/models/transfer_points.rb
+++ app/models/transfer_points.rb
@@ -1,9 +1,9 @@
-class TransferPoints
-  include ActionView::Helpers::TextHelper
-  include ActionView::Helpers::TranslationHelper
-  include ActionView::Helpers::UrlHelper
-  include ApplicationHelper
-  include Rails.application.routes.url_helpers
+class TransferPoints < ApplicationController
+  #include ActionView::Helpers::TextHelper
+  #include ActionView::Helpers::TranslationHelper
+  #include ActionView::Helpers::UrlHelper
+  #include ApplicationHelper
+  #include Rails.application.routes.url_helpers
   
   @@template = ERB.new File.read(File.expand_path "app/views/user_mailer/transfer_points_email.html.erb")
   
