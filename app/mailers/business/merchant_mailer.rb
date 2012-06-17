cea4f491fe9ce9aa2bd741acf24db939321bdb22
Paul Chan <paul.chan@justforyourfriends.com>
2012-06-17 02:21:51 -0400

    latest changes

--- app/mailers/business/merchant_mailer.rb
+++ app/mailers/business/merchant_mailer.rb
@@ -2,7 +2,7 @@ require 'util/common'
 
 module Business
   class MerchantMailer < ActionMailer::Base
-    default :from => "KICKBAK <notification@getkickbak.com>"
+    default :from => "KICKBAK <mail@getkickbak.com>"
     def contact_email(contact)
       @contact = contact
       mail(:from => "#{@contact.name} <#{@contact.email}>", :to => 'business_help@getkickbak.com', :subject => @contact.topic)
