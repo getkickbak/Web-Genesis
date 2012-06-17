639db1b425f202cabc9f8c33be8c8fbc1429d20e
Paul Chan <paul.chan@justforyourfriends.com>
2012-06-17 01:57:16 -0400

    latest changes

--- app/mailers/business/merchant_mailer.rb
+++ app/mailers/business/merchant_mailer.rb
@@ -11,12 +11,20 @@ module Business
     def summary_newsletter_email(merchant, stats)
       @merchant = merchant
       @stats = stats
-      mail(:to => 'wayofdragon@gmail.com', :subject => (I18n.t("business.mailer.email_subject_summary_newsletter") % [@merchant.name]))
+      to_email = merchant.email
+      if merchant.role == "test"
+        to_email = "paul.chan@getkickbak.com"
+      end
+      mail(:to => to_email, :subject => (I18n.t("business.mailer.email_subject_summary_newsletter") % [@merchant.name]))
     end
     
     def invoice_email(invoice)
       @invoice = invoice
-      mail(:to => 'wayofdragon@gmail.com', :subject => (I18n.t("business.mailer.email_subject_invoice") % [@invoice.invoice_id]))
+      to_email = invoice.merchant.email
+      if merchant.role == "test"
+        to_email = "paul.chan@getkickbak.com"
+      end
+      mail(:to => to_email, :subject => (I18n.t("business.mailer.email_subject_invoice") % [@invoice.invoice_id]))
     end
   end
 end 
