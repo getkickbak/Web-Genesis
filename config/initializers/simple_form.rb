d6b4adeae9520f57f6c224b4b930e9d004f0ebb6
Eric Chan <eric.chan@justformyfriends.com>
2012-07-19 15:22:17 -0400

    Revamp Merchant Site with Store bought theme

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -45,7 +45,7 @@ SimpleForm.setup do |config|
     b.use :error, :wrap_with => { :tag => :span, :class => :error }
   end
 
-  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+  config.wrappers :bootstrap, :tag => 'div', :class => 'control-group field', :error_class => 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
