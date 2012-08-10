30895fe17472b5ba61e5f01a9d5f806caa5323ae
Paul Chan <paul.chan@justforyourfriends.com>
2012-08-10 16:14:33 -0400

    latest changes to fix resque problems

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,11 @@
-require 'resque/server'
+require 'resque_scheduler'
+require 'resque_scheduler/server'
+
 ENV["REDISTOGO_URL"] ||= YAML.load_file("#{Rails.root}/config/properties.yml")[Rails.env]["REDIS_URL"]
 uri = URI.parse(ENV["REDISTOGO_URL"])
 
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-require 'resque_scheduler'
-
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
   
 Resque.schedule = YAML.load_file("#{Rails.root}/config/resque_schedule.yml")
+
+Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
