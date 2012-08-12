ac2c3226bbcd4af7edbd8746d0e5f73477d46692
Paul Chan <paul.chan@justforyourfriends.com>
2012-08-12 00:58:44 -0400

    bug fixes

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::CustomersController < ApplicationController
     
     winner_records = EarnPrizeRecord.all(:fields => [:user_id, :points, :created_ts], EarnPrizeRecord.merchant.id => params[:merchant_id], :points.gt => 1, :created_ts.gte => Date.today.at_beginning_of_month.to_time)
     winner_ids = []
-    winner_id_to_points = {}
+    winner_id_to_record = {}
     winner_records.each do |winner_record|
       user_id = winner_record[:user_id]
       winner_ids << user_id
