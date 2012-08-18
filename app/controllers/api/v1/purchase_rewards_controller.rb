4966af3a916342c9697b78b24b839ad1db470414
Paul Chan <paul.chan@justforyourfriends.com>
2012-08-18 03:26:37 -0400

    fix Eric's bug

--- app/controllers/api/v1/purchase_rewards_controller.rb
+++ app/controllers/api/v1/purchase_rewards_controller.rb
@@ -291,7 +291,7 @@ class Api::V1::PurchaseRewardsController < ApplicationController
           prize_trans_record.user = current_user
           prize_trans_record.save
           next_badge = Common.find_next_badge(@badges.to_a, @customer.badge)
-          if (@customer.next_badge_visits == next_badge.visits) && (@customer.badge.id != next_badge.id)
+          if (@customer.next_badge_visits >= next_badge.visits) && (@customer.badge.id != next_badge.id)
             adjustment_ratio = APP_PROP["BADGE_REBATE_RATE"] / (100 - APP_PROP["BADGE_REBATE_RATE"])
             badge_prize_points_average = (reward_model.avg_spend * next_badge.visits * adjustment_ratio / reward_model.price_per_prize_point).to_i
             badge_prize_points_diff = badge_prize_points_average / 2
