9a0119f5834773b0c65fe81468a156d98a47c640
Paul Chan <paul.chan@justforyourfriends.com>
2012-08-20 11:29:50 -0400

    error page revamp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,27 +42,39 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def get_template
+    template = "/error/error.html.erb"
+    if !Domain.matches?(request)
+      if request.subdomain == 'merchant'
+        template = "/error/business/error.html.erb"
+      else
+        template = "/error/admin/error.html.erb"
+      end
+    end
+    return template  
+  end
+  
   def render_unauthorized(exception)
-    handle_error(t("errors.messages.exception.http_401").split('\n'), "/error/error.html.erb", :status => 401)  
+    handle_error(t("errors.messages.exception.http_401_headline"), t("errors.messages.exception.http_401").split('\n'), get_template, :status => 401)  
   end
   
   def render_not_found(exception)
-    handle_error(t("errors.messages.exception.http_404").split('\n'), "/error/error.html.erb", :status => 404)
+    handle_error(t("errors.messages.exception.http_404_headline"), t("errors.messages.exception.http_404").split('\n'), get_template, :status => 404)
   end
 
   def render_error(exception)
     logger.error(exception)
-    handle_error(t("errors.messages.exception.http_500").split('\n'), "/error/error.html.erb", :status => 500)
+    handle_error(t("errors.messages.exception.http_500_headline"), t("errors.messages.exception.http_500").split('\n'), get_template, :status => 500)
   end
   
-  def handle_error(message, template, params)
+  def handle_error(headline, message, template, params)
     error = {
       :success => false,
       :message => message
     }
     status = params[:status] || :not_found
     respond_to do |format|
-      format.html { render :template => template, :locals => { :messages => message }, :status => status }
+      format.html { render :template => template, :locals => { :headline => headline, :messages => message }, :status => status }
       format.xml  { render :xml  => error.to_xml, :status => status }
       format.json { render :json => error.to_json, :status => status }
       format.yaml { render :text => error.to_yaml, :status => status }
