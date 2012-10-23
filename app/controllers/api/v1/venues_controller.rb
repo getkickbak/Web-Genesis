a1d34546397e7e644188522096b89a8916f78c12
Paul Chan <paul.chan@justforyourfriends.com>
2012-10-23 15:21:21 -0400

    latest changes

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -82,7 +82,6 @@ class Api::V1::VenuesController < ApplicationController
     authorize! :read, Venue
     
     if params[:image].blank?
-      logger.info("Cannot share photo, image is blank")
       respond_to do |format|
         #format.xml  { render :xml => @referral, :status => :created, :location => @referral }
         format.json { render :json => { :success => false, :message => t("api.photo_blank").split('\n') } }
@@ -99,6 +98,7 @@ class Api::V1::VenuesController < ApplicationController
       @photo_url = ::Common.generate_full_temp_file_path(filename)
       @upload_token = String.random_alphanumeric
       session[:photo_upload_token] = @upload_token
+      logger.info("Setting photo_upload_token: #{session[:photo_upload_token]}")
       render :template => '/api/v1/venues/share_photo'
     end
   end  
