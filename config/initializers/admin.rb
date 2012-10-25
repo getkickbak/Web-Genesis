cc364feb5baaa715084126df07fa8336e3c9f045
Paul Chan <paul.chan@justforyourfriends.com>
2012-10-25 17:12:44 -0400

    set serve_static_assets = true authenticate resque-web

--- config/initializers/admin.rb
+++ config/initializers/admin.rb
@@ -1,5 +1,6 @@
 class CanAccessResque
   def self.matches?(request)
+    return false if !request.env['warden'].authenticate?
     current_staff = request.env['warden'].user(:scope => :staff)
     return false if current_staff.blank?
     StaffAbility.new(current_staff).can? :manage, Resque
