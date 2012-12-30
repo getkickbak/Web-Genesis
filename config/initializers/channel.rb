5cf891eb42ac38d4deabc1b08df9f18d937f465e
Paul Chan <paul.chan@getkickbak.com>
2012-12-30 16:22:38 -0500

    bug fix

--- config/initializers/channel.rb
+++ config/initializers/channel.rb
@@ -1,9 +1,10 @@
+hash = YAML.load(File.new(File.expand_path("config/database.yml", Rails.root)))
 DataMapper.setup(:default, hash[Rails.env])
 exists = DataMapper.repository(:default).adapter.storage_exists?('venues')
 if exists
-  venues_info = Venues.all(:fields => [:id])
-  venues_info.each do |venue_info|
-    device_count = Device.count(:merchant_venue_id => venue_info.id)
+  venues = Venue.all
+  venues.each do |venue|
+    device_count = Device.count(:merchant_venue => venue.id)
     device_count.times do |x|
       Channel.add
     end
