a6af36974779f0638e8ae8255d8ade7cd4cbaa7e
Eric Chan <eric.chan@getkickbak.com>
2012-12-30 21:38:10 -0500

    latest bug fix

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,26 +4,38 @@ class Channel
   @@groups = ["ChannelGroup-1"]
   @@count = 0
   @@group_size = 5
+  @@groups_mutex_name = "ChannelGroups"
   
   def self.get_group
-    @@groups[Random.rand(@@groups.length)]
+    begin
+      mutex = CacheMutex.new(@@groups_mutex_name, Cache.memcache)
+      acquired = mutex.acquire
+      @@groups[Random.rand(@@groups.length)]
+    ensure
+      mutex.release if ((defined? mutex) && !mutex.nil?)
+    end
   end
   
   def self.add
     begin
       group = @@groups.last
-      mutex = CacheMutex.new(group, Cache.memcache)
-      acquired = mutex.acquire
+      group_mutex = CacheMutex.new(group, Cache.memcache)
+      acquired = group_mutex.acquire
       @@count = @@count + 1
-      group = @@count / @@group_size
-      if group > @@groups.length
-        @@groups << "ChannelGroup-#{group}"
+      group_count = @@count / @@group_size
+      if group_count > @@groups.length
+        groups_mutex = CacheMutex.new(@@groups_mutex_name, Cache.memcache)
+        acquired = groups_mutex.acquire
+        @@groups << "ChannelGroup-#{group_count}"
       elsif @@count == (@@group_size + 1) 
-        group = group + 1
-        @@groups << "ChannelGroup-#{group}"
+        group_count = group_count + 1
+        groups_mutex = CacheMutex.new(@@groups_mutex_name, Cache.memcache)
+        acquired = groups_mutex.acquire
+        @@groups << "ChannelGroup-#{group_count}"
       end
       channel = "/tmp/channel_#{@@count}"
       system("mkfifo #{channel}")
+      group = @@groups.last
       channels = @@free_list[group]
       if channels.nil?
         channels = @@free_list[group] = {}
@@ -34,7 +46,8 @@ class Channel
         Rails.logger.info("channels in this group: #{@@free_list[item[0]]}")
       end
     ensure
-      mutex.release if ((defined? mutex) && !mutex.nil?)
+      groups_mutex.release if ((defined? groups_mutex) && !groups_mutex.nil?)
+      group_mutex.release if ((defined? group_mutex) && !group_mutex.nil?)
     end
   end
   
