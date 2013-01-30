1880c038830bbedc4e0e2aad081a732b24da1335
Paul Chan <paulchan@Paul-Chans-MacBook-Pro.local>
2013-01-29 22:44:00 -0500

    latest changes

--- app/jobs/create_promotion.rb
+++ app/jobs/create_promotion.rb
@@ -20,44 +20,59 @@ module CreatePromotion
       elsif count > max
         n = count/max + 1
       end  
-      logger.info("Promotion message requires #{n} iterations")
+      logger.info("Promotion(#{promotion.id}) requires #{n} iterations")
       for i in 0..n-1
         logger.info("Sending iteration #{i+1}")
         start = i*max
         customers = Customer.all(:fields => [:user_id], :merchant => promotion.merchant, :offset => start, :limit => max)
         user_list = []
+        device_user_list = []
         customers.each do |customer|
           user_list << customer.user_id 
         end
-        devices = UserDevice.all(:fields => [:device_id, :user_id], :user_id => user_list)
-        device_list = []
-        device_user_list = []
-        devices.each do |device|
-          device_list << device.device_id
-          device_user_list << device.user_id
-        end
-        #logger.debug("Device list: #{device_list}")
         logger.info("Sending mobile notifications")
-        message = "#{promotion.merchant.name} - #{promotion.message}"
-        ret = push.create_message(promotion.merchant.id, message, promotion.start_date, device_list)
-        logger.info("Response body: #{ret.response}")
-        if ret.success?
-          logger.info("Completed iteration #{i+1}")
-        else  
-          now = Time.now
-          logger.info("Failed to complete iteration #{i+1}")
-          logger.info("Create Promotion failed to send message for Promotion(#{promotion.id}), Error Code(#{ret.response[:status_code]}) at #{now.strftime("%a %m/%d/%y %H:%M %Z")}")
+        if user_list.length > 0
+          devices = UserDevice.all(:fields => [:device_id, :user_id], :user_id => user_list)
+          device_list = []
+          devices.each do |device|
+            device_list << device.device_id
+            device_user_list << device.user_id
+          end
+          if device_list.length > 0
+            #logger.debug("Device list: #{device_list}")
+            message = "#{promotion.merchant.name} - #{promotion.message}"
+            ret = push.create_message(promotion.merchant.id, message, promotion.start_date, device_list)
+            logger.info("Response body: #{ret.response}")
+            if ret.success?
+              logger.info("Sending mobile notifications - complete for iteration #{i+1}")
+            else  
+              now = Time.now
+              logger.info("Failed to complete iteration #{i+1}, Error Code(#{ret.response[:status_code]})")
+            end
+          else
+            logger.info("No mobile notifications to send for iteration #{i+1}")
+          end
+        else
+          logger.info("No mobile notifications to send for iteration #{i+1}")  
         end
-        logger.info("Sending mobile notifications - complete")
         logger.info("Sending emails")
         email_user_list = user_list - device_user_list
-        email_users = User.all(:fields => [:name], :id => email_user_list)
-        email_users.each do |user|
-          if user.subscription.email_notif
-            UserMailer.promotion_email(user, promotion).deliver
-          end  
+        if email_user_list.length > 0
+          email_users = User.all(:fields => [:id, :email], :id => email_user_list)
+          subscriptions = Subscription.all(:fields => [:user_id, :email_notif], :user_id => email_user_list)
+          user_id_to_subscription = []
+          subscriptions.each do |subscription|
+            user_id_to_subscription[subscription.user_id] = subscription
+          end
+          email_users.each do |user|
+            if user_id_to_subscription[user.id].email_notif
+              UserMailer.promotion_email(user, promotion).deliver
+            end  
+          end
+          logger.info("Sending emails - complete for iteration #{i+1}")
+        else
+          logger.info("No emails to send for iteration #{i+1}")  
         end
-        logger.inf("Sending emails - complete")
       end
       begin  
         Promotion.transaction do
