042853e4a6e5ac98affed6614d254da63a88f0c6
Paul Chan <paulchan@Paul-Chans-MacBook-Pro.local>
2013-02-03 02:36:11 -0500

    latest changes

--- app/models/request.rb
+++ app/models/request.rb
@@ -45,7 +45,8 @@ class Request
       c = lambda {
         return DataMapper.repository(:default).adapter.select(
           "SELECT id, round( 6371000 * acos( cos( radians(?) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(?) ) + sin( radians(?) ) * sin( radians( latitude ) ) ), 1) AS distance
-          FROM requests WHERE type = ? AND status = ? AND abs(frequency1 - ?) <= 5 AND abs(frequency2 - ?) <= 5 AND abs(frequency3 - ?) <= 5 AND distance <= 100 AND deleted_ts IS NULL
+          FROM requests WHERE type = ? AND status = ? AND abs(frequency1 - ?) <= 5 AND abs(frequency2 - ?) <= 5 AND abs(frequency3 - ?) <= 5 AND deleted_ts IS NULL
+          HAVING distance < 100
           ORDER BY distance
           ASC LIMIT 0,1", request_info[:latitude], request_info[:longitude], request_info[:latitude], request_info[:type], Request::Statuses.index(:pending)+1, request_info[:frequency1], request_info[:frequency2], request_info[:frequency3]
         )
