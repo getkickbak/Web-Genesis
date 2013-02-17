15a26311da5be9c366a9e7f66fb0e4cf9dd06e26
Paul Chan <paulchan@Paul-Chans-MacBook-Pro.local>
2013-02-17 05:55:23 -0500

    latest changes

--- public/javascripts/desktop/src/business/credit_card.js
+++ public/javascripts/desktop/src/business/credit_card.js
@@ -10,4 +10,16 @@ $(document).ready($(function() {
 			}
 		})
 	})
+	$('a[id^="cancel-update-credit-card-"]').each(function( index ) {
+		$(this).click(function() {
+			var card_id = $(this).attr('card_id');
+			var display = $('#edit_credit_card_'+card_id).css('display');
+			if (display == 'none') {
+				$('#edit_credit_card_'+card_id).show();
+			} else {
+				$('#edit_credit_card_'+card_id).hide();
+			}
+		})
+	})
+	
 }))
