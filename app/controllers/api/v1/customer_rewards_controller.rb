4ed3be0dc67e9106736b8ddb78842147cca4f578
Paul Chan <paulchan@Paul-Chans-MacBook-Pro.local>
2013-02-27 10:13:42 -0500

    latest changes

--- app/controllers/api/v1/customer_rewards_controller.rb
+++ app/controllers/api/v1/customer_rewards_controller.rb
@@ -98,7 +98,7 @@ class Api::V1::CustomerRewardsController < Api::V1::BaseApplicationController
         logger.info("Venue(#{@venue.id}) failed to complete Request(#{@request.id})")
         request_data = JSON.parse(@request.data)
         if request_data["message"]
-          message = t("api.customer_rewards.redeem_mismatch").split(/\n/)
+          message = request_data["message"]
         else
           message = (t("api.customer_rewards.redeem_failure") % [@reward.mode == :reward ? t("api.reward") : t("api.prize")]).split(/\n/)  
         end  
