b2f6d100e3402f8a6d1e8afef5bacb7357cf5315
Paul Chan <paulchan@Paul-Chans-MacBook-Pro.local>
2013-02-28 16:32:03 -0500

    Set :persistent => false in fog_credentials to fix the S3 timeout issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIBKLCOJMK2OWLHUQ',       # required
-    :aws_secret_access_key  => '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'       # required
+    :aws_secret_access_key  => 'rmCQKWmtzAAeRwAPi5f9ikll5WHMdP0J0ncqf2NI',      # required
+    :persistent             => false
   }
   config.fog_directory  = "#{APP_PROP["AMAZON_PHOTOS_BUCKET"]}"                     # required
   config.fog_host       = "http://#{APP_PROP["PHOTO_HOST"]}"            # optional, defaults to nil
