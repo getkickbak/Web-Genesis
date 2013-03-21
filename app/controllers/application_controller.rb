d37744d04c4ec42c4700c0a0ece54b51536c89f8
Paul Chan <paul.chan@getkickbak.com>
2013-03-21 01:47:26 -0400

    latest changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,9 +64,7 @@ class ApplicationController < ActionController::Base
 
   def secure_with_ssl
     if Rails.env.production?
-      if (request.subdomain == 'manage'  || request.subdomain == 'merchant') && request.protocol != 'https'
-        logger.info("subdomain: #{request.subdomain}")
-        logger.info("protocol: #{request.protocol}")
+      if (request.subdomain == 'manage'  || request.subdomain == 'merchant') && request.protocol != 'https://'
         redirect_to :protocol => 'https'
       end
     end
