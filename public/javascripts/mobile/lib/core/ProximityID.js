0e20f23d959d6524b53cf8d903791ba6089c3581
Eric Chan <eric.chan@getkickbak.com>
2013-06-28 17:56:06 -0400

    Bug fixes

--- public/javascripts/mobile/lib/core/ProximityID.js
+++ public/javascripts/mobile/lib/core/ProximityID.js
@@ -129,67 +129,24 @@ else
             if (!me.context)
             {
                me.context = new webkitAudioContext();
-               me.context.createGainNode();
+               me.gainNode = context.createGain();
+               me.gainNode.connect(context.destination);
             }
-            //
-            // To give loading mask a chance to render
-            //
-            //Ext.defer(function()
+
+            getFreqs();
+            var s_vol = (Ext.os.is('Desktop')) ? (Genesis.constants.s_vol / 100) : 1.0;
+            // Reduce the volume.
+            me.gainNode.gain.value = s_vol;
+            
+            for ( i = 0; i < me.freqs.length; i++)
             {
-               // Number of samples to generate on each call to generateAudio.
-               // Legal values are 256, 512, 1024, 2048, 4096, 8192, 16384.
-               const BUFFER_SIZE = 16384;
-
-               // Number of output channels. We want stereo, hence 2 (though 1 also works??).
-               const NUM_OUTPUTS = 1;
-
-               // We only want to *generate* audio, so our node has no inputs.
-               //const NUM_INPUTS = 0; Results in horrible noise in Safari 6
-               const NUM_INPUTS = 1;
-
-               me.currentPhase = [];
-               me.phaseIncrement = [];
-               // Create a source node
-               me.node = me.context.createJavaScriptNode(BUFFER_SIZE, NUM_INPUTS, NUM_OUTPUTS);
-
-               getFreqs();
-               for ( i = 0; i < me.freqs.length; i++)
-               {
-                  me.currentPhase[i] = 0.0;
-                  me.phaseIncrement[i] = 2 * Math.PI * me.freqs[i] / me.sampleRate;
-                  console.debug("Freq " + me.freqs[i]);
-               }
-               var s_vol = (Ext.os.is('Desktop')) ? (Genesis.constants.s_vol / 100) : 1.0;
-               me.node.onaudioprocess = function(e)
-               {
-                  // Get the left and right output buffers
-                  var left = e.outputBuffer.getChannelData(0);
-                  //var right = e.outputBuffer.getChannelData(1);
-
-                  // For each output sample
-                  var numSamples = left.length;
-
-                  for (var s = 0; s < numSamples; s++)
-                  {
-                     var val = 0.0;
-                     for (var j = 0; j < me.freqs.length; j++)
-                     {
-                        val += Math.sin(me.currentPhase[j]);
-                        // Increment the phase
-                        me.currentPhase[j] += me.phaseIncrement[j];
-                     }
-                     // Get a sine wave value
-                     val /= me.freqs.length;
-
-                     // Put it in the left and right buffer
-                     left[s] = s_vol * val;
-                     //right[i] = val;
-                  }
-                  console.debug("Injected " + numSamples + " Buffers");
-               };
-               win();
+               var osc = me.oscillators[i] = context.createOscillator();
+               osc.type = 0;
+               osc.frequency.value = me.freqs[i];
+               osc.connect(me.gainNode);
             }
-            //, 0.25 * 1000, this);
+
+            win();
          }
          else
          {
@@ -244,8 +201,11 @@ else
          }
          else if (me.node)
          {
-            // Connect the node to a destination, i.e. the audio output.
-            me.node.connect(me.context.destination);
+            for (var i = 0; i < me.freqs.length; i++)
+            {
+
+               me.oscillators[i].noteOn && me.oscillators[ii].noteOn(0);
+            }
             win(
             {
                freqs : me.freqs
@@ -265,10 +225,13 @@ else
             me.audio.currentTime = 0;
             delete me.audio;
          }
-         else if (me.node)
+         else if (me.oscillators)
          {
-            me.node.disconnect();
-            delete me.node;
+            for (var i = 0; i < me.freqs.length; i++)
+            {
+               me.oscillators[i].disconnect();
+            }
+            delete me.oscillators;
          }
       },
       setVolume : function(vol)
