da4afeba3a1ffd0b4841af7432eaf482a58bbf6e
Eric Chan <eric.chan@getkickbak.com>
2013-07-02 18:38:59 -0400

    Bug fixes

--- public/javascripts/build/Genesis/testing/worker/encoder.js
+++ public/javascripts/build/Genesis/testing/worker/encoder.js
@@ -1,10 +1,10 @@
 if ( typeof (importScripts) != 'undefined')
 {
-   importScripts('http://files.getkickbak.com/mobileWeb/build/KickBak/production/lib/libmp3lame.min.js');
+   importScripts('../lib/libmp3lame.min.js');
 }
 else
 {
-   Genesis.fn.checkloadjscssfile('http://files.getkickbak.com/mobileWeb/build/KickBak/production/lib/libmp3lame.min.js', "js", Ext.emptyFn);
+   Genesis.fn.checkloadjscssfile('../lib/libmp3lame.min.js', "js", Ext.emptyFn);
 }
 
 var mp3codec;
