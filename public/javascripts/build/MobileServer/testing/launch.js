0d5c23956aff9310c133e18a0d58cffd5086bf51
Eric Chan <eric.chan@getkickbak.com>
2013-07-16 03:38:55 -0400

    Bug fixes

---
+++ public/javascripts/build/MobileServer/testing/launch.js
@@ -0,0 +1,24 @@
+var singletonWindow;
+/**
+ * Listens and launches the window, using a reference
+ * to determine if a window already exists. If it does
+ * that window is focused, otherwise a new window
+ * is created and the reference stored for next time.
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function()
+{
+   chrome.app.window.create('index.html',
+   {
+      id : "MerKickBak",
+      singleton : true,
+      bounds :
+      {
+         width : 320,
+         height : 568
+      },
+      resizable : false
+   });
+}); 
