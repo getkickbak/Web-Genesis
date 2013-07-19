7df9b87a9292c957e478cb53ad2247ffa3bee527
Eric Chan <eric.chan@getkickbak.com>
2013-07-19 05:16:53 -0400

    Bug fixes

--- public/javascripts/mobileServer/startup.js
+++ public/javascripts/mobileServer/startup.js
@@ -1,5 +1,3 @@
-_build = "MobileWebServer";
-var phoneGapAvailable = false, serverHost, merchantMode = true, _application, _filesAssetCount = 0, debugMode = true;
 document.addEventListener("DOMContentLoaded", function(event)
 {
    var _frame = document.getElementById('merkickbak');
@@ -17,6 +15,4 @@ document.addEventListener("DOMContentLoaded", function(event)
       }
       console.debug("[" + e.target.id + "] permissionrequest: permission=" + e.permission + " " + ( allowed ? "allowed" : "DENIED"));
    }, false);
-
-   window.plugins.proximityID.init(0.5, 0.5);
 });
