0f9aa97ea49c21a9ed7f8e738b2516f6ff3fe199
Eric Chan <eric.chan@getkickbak.com>
2013-10-23 02:26:10 -0400

    Bug fixes

--- public/javascripts/mobile/app/controller/server/Settings.js
+++ public/javascripts/mobile/app/controller/server/Settings.js
@@ -309,7 +309,7 @@ Ext.define('Genesis.controller.server.Settings',
       var me = this, form = me.getSettingsPage(), db = Genesis.db.getLocalDB(), isNative = Genesis.fn.isNative();
 
       me.getMerchantDevice().setLabel(Genesis.fn.getPrivKey('venue'));
-      me.getDeviceID().setLabel('DeviceID' + '<div style="font-size:0.6em;line-height:1;">' + (isNative ? device.uuid : db['uuid']) + '</div>');
+      me.getDeviceID().setLabel('DeviceID' + '<div style="font-size:0.60em;line-height:1;">' + (isNative ? device.uuid : db['uuid']) + '</div>');
       me.getUtilitiesContainer()[debugMode ? 'show' : 'hide']();
       form.setValues(
       {
